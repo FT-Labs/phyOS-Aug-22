68f6977c2d6a7ca208beb18fb30ecad395e86b8a
phytech-r0 <phystecharda@gmail.com>
2022-06-19 21:53:15 +0000

    V1.0

--- profiledef.sh
+++ profiledef.sh
@@ -17,7 +17,4 @@ file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root/"]="0:0:744"
   ["/etc/skel/"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
 )
