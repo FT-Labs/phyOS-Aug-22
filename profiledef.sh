b1c84cf27b64df2c719406b6388e578e50e58853
Arda Atci <arda@phytech.io>
2022-06-24 16:41:06 +0300

    iso

--- profiledef.sh
+++ profiledef.sh
@@ -15,8 +15,11 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:600"
   ["/etc/polkit-1/rules.d"]="0:0:750"
   ["/etc/sudoers.d"]="0:0:750"
-  ["/root/"]="0:0:744"
-  ["/etc/skel/"]="0:0:755"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/etc/skel/"]="0:0:744"
+  ["/etc/skel/.local/bin/"]="0:0:755"
 )
