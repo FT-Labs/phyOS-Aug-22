cbf897bc15c9d730a137538121746a66f926c99f
Arda Atci <phystecharda@gmail.com>
2022-10-06 01:52:07 +0300

    new iso

--- profiledef.sh
+++ profiledef.sh
@@ -8,12 +8,14 @@ iso_application="PhyOS Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
+           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')   #xz for final image, compresses more than zstd
-#airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '1')  #zstd for debugging, burns iso fast (uses multi threads)
+#airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')   #xz for final image, compresses more than zstd
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '1')  #zstd for debugging, burns iso fast (uses multi threads)
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/etc/gshadow"]="0:0:600"
